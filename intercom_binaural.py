de8ffbb11c4a9d4b7732664b6dd483100d7accce
jesusss949 <55703490+jesusss949@users.noreply.github.com>
2019-11-25 16:59:09 +0100

    jesusss949
    Errores de sincronizacion arreglados

--- intercom_binaural.py
+++ intercom_binaural.py
@@ -1,11 +1,4 @@
-import sounddevice as sd
-import numpy as np
-import struct
-from intercom import Intercom
-from intercom_buffer import Intercom_buffer
 from intercom_bitplanes import Intercom_bitplanes
-
-
 
 if __debug__:
     import sys
